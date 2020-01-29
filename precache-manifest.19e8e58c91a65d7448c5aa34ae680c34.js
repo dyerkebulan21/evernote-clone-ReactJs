981aa8d36a30cb03d72a25c53e2b4c401e63fc4f
dyerkebulan21 <dyerkebulan21@gmail.com>
2020-01-29 19:00:33 +0600

    Updates

---
+++ precache-manifest.19e8e58c91a65d7448c5aa34ae680c34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d486da9f7aec2c7c8770d4a298739c2",
+    "url": "/evernote-clone-ReactJs/index.html"
+  },
+  {
+    "revision": "24fcdd3c7bbad86b4786",
+    "url": "/evernote-clone-ReactJs/static/css/main.60d5545d.chunk.css"
+  },
+  {
+    "revision": "a32ae997a494b4211ec6",
+    "url": "/evernote-clone-ReactJs/static/js/2.adcaedd0.chunk.js"
+  },
+  {
+    "revision": "2a196d222152c45027e5b2ccfe72e93e",
+    "url": "/evernote-clone-ReactJs/static/js/2.adcaedd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "24fcdd3c7bbad86b4786",
+    "url": "/evernote-clone-ReactJs/static/js/main.0ec8372e.chunk.js"
+  },
+  {
+    "revision": "98d3215f56b547877242",
+    "url": "/evernote-clone-ReactJs/static/js/runtime-main.6d6ee7a6.js"
+  }
+]);
